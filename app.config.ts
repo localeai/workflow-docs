26975014ae6e54d9975f1c760b4301f610a1f021
haxzie <musthu.gm@gmail.com>
2024-07-01 12:05:09 +0530

    Fix cover image url

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
   docus: {
     title: 'Locale Docs',
     description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'https://docs.locale.ai/cover.png',
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
