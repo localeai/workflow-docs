1cfdc189beab35654218c88903f645a21241aecb
haxzie <musthu.gm@gmail.com>
2024-07-02 16:10:36 +0530

    Add gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,13 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ["@nuxt-themes/docus"],
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts"],
-  css: ['~/assets/styles/main.css'],
+  modules: ["@nuxt/fonts", "nuxt-gtag"],
+  css: ["~/assets/styles/main.css"],
   fonts: {
     families: [{ name: "Inter", provider: "google" }],
+  },
+  // google tag manager
+  gtag: {
+    id: "GTM-P2ZSKZP",
   },
 });
